037c19b7f373636b6e0553b26243101d073f5477
Adri Wischmann <a.wischmann@iotnederland.nl>
2021-03-16 18:46:52 +0100

    add attendancyAddress

--- code/attendee.js
+++ code/attendee.js
@@ -2,16 +2,17 @@
 // Attendee attend-event-app
 //////////////////////////////////////////////////////////
 
-const {
-  channelRoot,
-  mamFetch,
-  TrytesHelper,
-} = require("@iota/mam-chrysalis.js");
+const { mamFetch, TrytesHelper } = require("@iota/mam-chrysalis.js");
 const fs = require("fs");
 const prompt = require("prompt-sync")({ sigint: true });
 const colors = require("colors");
+const { Console } = require("console");
 
 const node = "https://api.hornet-0.testnet.chrysalis2.com";
+const commonSideKey =
+  "SSACOMMONKEY999999999999999999999999999999999999999999999999999999999999999999999";
+let publicEventRoot = "";
+let attendancyAddress = "";
 
 function readQR() {
   // Try and load the QR-root from file
@@ -21,16 +22,12 @@ function readQR() {
   } catch (err) {}
 }
 
-console.log("SSA-attendee-app".cyan);
-let readQRcode = readQR();
-console.log(`QRcode from file = ${readQRcode}`.yellow);
-let eventQR = prompt("Event QR-code (*=savedversion): ");
-if (eventQR === "*") eventQR = readQRcode;
-
 // readQRmam
 async function readQRmam(qrRoot) {
   const mode = "restricted";
   const sideKey = "DATE";
+  let rootValue = "NON";
+  let indexationKey = "";
 
   console.log("Fetching from tangle, please wait...");
   console.log(`Node : ${node}`.yellow);
@@ -42,15 +39,45 @@ async function readQRmam(qrRoot) {
   console.log("Fetching from tangle, please wait...");
   const fetched = await mamFetch(node, qrRoot, mode, sideKey);
   if (fetched) {
-    console.log("Fetched", JSON.parse(TrytesHelper.toAscii(fetched.message)));
+    let fMessage = JSON.parse(TrytesHelper.toAscii(fetched.message));
+    console.log("Fetched : ", fMessage);
+    rootValue = fMessage.root;
+    indexationKey = fMessage.indexation;
+    console.log(`Message.root : ${rootValue}`);
+    console.log(`Message.indexation : ${indexationKey}`);
+  } else {
+    console.log("Nothing was fetched from the MAM channel");
+  }
+  console.log("MAMdata ===================".red);
+  console.log(`fetched : ${fetched.message}`.green);
+  console.log("============================".yellow);
+  publicEventRoot = rootValue;
+  attendancyAddress = indexationKey;
+}
+
+// readPublicEventInfo
+async function readPublicEventInfo(publicEventRoot) {
+  const mode = "restricted";
+  const sideKey = commonSideKey;
+
+  console.log("Fetching from tangle, please wait...");
+  console.log(`Node : ${node}`.yellow);
+  console.log(`EventRoot : ${publicEventRoot}`.yellow);
+  console.log(`mode : ${mode}`.yellow);
+  console.log(`sideKey : ${sideKey}`.yellow);
+
+  // Try fetching from MAM
+  console.log("Fetching from tangle, please wait...");
+  const fetched = await mamFetch(node, publicEventRoot, mode, sideKey);
+  if (fetched) {
+    let fMessage = JSON.parse(TrytesHelper.toAscii(fetched.message));
+    console.log("Fetched : ", fMessage);
   } else {
     console.log("Nothing was fetched from the MAM channel");
   }
   console.log("MAMdata ===================".red);
   console.log(`fetched : ${fetched.message}`.green);
 }
-
-// readPublicEventInfo
 
 // presentEventInfo
 
@@ -60,4 +87,14 @@ async function readQRmam(qrRoot) {
 
 // compileVerifierQR
 
-readQRmam(eventQR);
+console.log("SSA-attendee-app".cyan);
+let readQRcode = readQR();
+console.log(`QRcode from file = ${readQRcode}`.yellow);
+let eventQR = prompt("Event QR-code (*=savedversion): ");
+if (eventQR === "*") eventQR = readQRcode;
+
+readQRmam(eventQR).then(function () {
+  if (publicEventRoot === "NON") {
+    console.log("Invalid eventRoot-address".red);
+  } else readPublicEventInfo(publicEventRoot);
+});
