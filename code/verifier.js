16aa6fd61f74eeaeba897504c588bd4dc7980732
Adri Wischmann <a.wischmann@iotnederland.nl>
2021-03-23 15:50:26 +0100

    add show eventInformation

--- code/verifier.js
+++ code/verifier.js
@@ -3,21 +3,9 @@
 // (c) A.J. Wischmann 2021
 //////////////////////////////////////////////////////////
 
-const {
-  mamFetch,
-  TrytesHelper,
-  channelRoot,
-  createChannel,
-} = require("@iota/mam-chrysalis.js");
+const { mamFetch, TrytesHelper } = require("@iota/mam-chrysalis.js");
 const { Converter } = require("@iota/iota.js");
-const {
-  hexToBuffer,
-  sha256,
-  encrypt,
-  bufferToUtf8,
-  utf8ToBuffer,
-  bufferToHex,
-} = require("eccrypto-js");
+const { sha256, utf8ToBuffer, bufferToHex } = require("eccrypto-js");
 // const crypto = require("crypto");
 const luxon = require("luxon");
 const fs = require("fs");
@@ -31,6 +19,7 @@ let publicEventRoot = "";
 let verificationQR = "";
 let attendeeToken = "";
 let qrTime = "";
+let eventInformation = "";
 
 async function hashHash(hashData) {
   let element = utf8ToBuffer(hashData);
@@ -66,11 +55,11 @@ async function checkQR(code) {
   if (crccode == crcValue) {
     publicEventRoot = rootcode;
     attendeeToken = await hashHash(idstring);
-    console.log(`attendeeToken :${attendeeToken}`);
+    // console.log(`attendeeToken :${attendeeToken}`);
     qrTime = luxon.DateTime.fromMillis(+timecode);
     nowTime = luxon.DateTime.now();
     let timeDiff = nowTime.diff(qrTime);
-    if (timeDiff.as(`minutes`) > 210) console.log(`Suspicious behaviour :`.red);
+    if (timeDiff.as(`minutes`) > 10) console.log(`Suspicious behaviour :`.red);
     console.log(
       `QR-code was generated ${timeDiff.as(`minutes`)} minutes ago`.yellow
     );
@@ -80,9 +69,47 @@ async function checkQR(code) {
   return false;
 }
 
-// readEventInfo
-// readAttendeeList -till ClosedMessage
-// checkAttendeeOnList
+async function readPublicEventInfo(publicEventRoot) {
+  const mode = "restricted";
+  const sideKey = commonSideKey;
+
+  console.log("Fetching from tangle with this information :");
+  console.log(`Node : ${node}`.yellow);
+  console.log(`EventRoot : ${publicEventRoot}`.yellow);
+  console.log(`mode : ${mode}`.yellow);
+  console.log(`sideKey : ${sideKey}`.yellow);
+
+  // Try fetching from MAM
+  console.log("Fetching from tangle, please wait...");
+  const fetched = await mamFetch(node, publicEventRoot, mode, sideKey);
+  if (fetched) {
+    let fMessage = JSON.parse(TrytesHelper.toAscii(fetched.message));
+    //DEBUGINFO
+    // console.log("MAMdata ===================".red);
+    // console.log(`fetched : ${fetched.message}`.green);
+    return fMessage;
+  }
+  console.log("Nothing was fetched from the MAM channel".red);
+  return "error";
+}
+
+function presentEventInfo(eventRecord) {
+  console.log("=================================".red);
+  console.log("Event :".cyan);
+  console.log(`Name : ${eventRecord.eventname}`);
+  console.log(`Date : ${eventRecord.eventdate}`);
+  console.log(`Time : ${eventRecord.eventtime}`);
+  console.log(`Location : ${eventRecord.eventloc}`);
+  console.log("Organised by :".cyan);
+  console.log(`Organisation : ${eventRecord.orgname}`);
+  console.log(`Address : ${eventRecord.orgaddress}`);
+  console.log(`Zipcode : ${eventRecord.orgzip}`);
+  console.log(`City : ${eventRecord.orgcity}`);
+  console.log(`Tel.nr. : ${eventRecord.orgtel}`);
+  console.log(`E-mail : ${eventRecord.orgmail}`);
+  console.log(`WWW : ${eventRecord.orgurl}`);
+  console.log(`DID : ${eventRecord.orgdid}`);
+}
 
 async function run() {
   console.log("SSA-verifier-app".cyan);
@@ -93,7 +120,14 @@ async function run() {
     console.log("This QR-code is not correct! - Verification aborted.".red);
     return;
   } else {
-    const verifyQR = prompt("Attendee verifier-QR-code : ");
+    // readEventInfo
+    eventInformation = await readPublicEventInfo(publicEventRoot);
+    // show eventinfo
+    if (!eventInformation == "error") {
+      presentEventInfo(eventInformation);
+    }
+    // readAttendeeList -till ClosedMessage
+    // checkAttendeeOnList
   }
 }
 
